169ac5d49f3a4f42906797f2ddb097730bb79de1
Tran B.V. Son <tranbavinhson@gmail.com>
2017-11-13 00:57:46 +0700

    fix

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
   # sign in
   def create
@@ -10,6 +12,13 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response "Unauthorized", false, {}, :unauthorized
     end
+  end
+
+  # log out
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response "Log out Successfully", true, {}, :ok
   end
 
   private
@@ -23,6 +32,15 @@ class Api::V1::SessionsController < Devise::SessionsController
       return @user
     else
       json_response "Cannot get User", false, {}, :failure
+    end
+  end
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+    if @user
+      return @user
+    else
+      json_response "Invalid Token", false, {}, :failure
     end
   end
 end
