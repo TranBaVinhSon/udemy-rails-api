0137fcc52a38e75a9bd3a8ffdd632a6ac06de97c
Tran B.V. Son <tranbavinhson@gmail.com>
2017-11-12 16:47:03 +0700

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       devise_scope :user do
         post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
       end
     end
   end
