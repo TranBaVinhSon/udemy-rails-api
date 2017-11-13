cbe579d94ebb69862c7f06790e11a848da25d11d
Tran B.V. Son <tranbavinhson@gmail.com>
2017-11-13 20:10:53 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
         post "sign_in", to: "sessions#create"
         delete "log_out", to: "sessions#destroy"
       end
+
+      post "facebook", to: "users#facebook"
     end
   end
 end
